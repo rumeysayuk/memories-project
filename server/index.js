c31e05058bca154a3f98b3fe83b60f942a9434fd
rumeysa.yuk <rumeysayuk90@gmail.com>
2022-06-29 23:53:14 +0300

    comments

--- server/index.js
+++ server/index.js
@@ -4,12 +4,17 @@ import bodyParser from "body-parser"
 import cors from "cors"
 import databaseConnectionHelper from "./helpers/database/databaseConnectionHelper.js"
 import routes from './routes/index.js';
+
 const app = express()
 databaseConnectionHelper();
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
 app.use('/api', routes);
+
+app.get("/", (req, res) => {
+   res.send("App is running")
+})
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
